571266d0bc75b9151d233cf3c8472b2a3d60e04b
Shree Chakra Electric Motors <shreechakra.e.m@gmail.com>
2025-12-03 06:01:52 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,4 +1,11 @@
 // This configuration is intentionally left blank.
 // It will be populated automatically by Firebase App Hosting.
-const firebaseConfig = {};
+const firebaseConfig = {
+  "projectId": "studio-6332033724-be73d",
+  "appId": "1:302866980866:web:58df9a7b1bb0bc9083c71b",
+  "apiKey": "AIzaSyBQ51OsRb2OD1d_X9OoacJrrzh1oKG3i1s",
+  "authDomain": "studio-6332033724-be73d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "302866980866"
+};
 export default firebaseConfig;
