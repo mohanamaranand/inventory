89801bca18940ffa4633f96fc97f95c2619d243d
Shree Chakra Electric Motors <shreechakra.e.m@gmail.com>
2025-12-09 11:29:16 +0000

    inventory page and custom page opens in this simulator browser with in f

--- src/app/customers/page.tsx
+++ src/app/customers/page.tsx
@@ -4,24 +4,13 @@ import { Header } from "@/components/layout/header";
 import { Skeleton } from "@/components/ui/skeleton";
 import { CustomerClient } from "@/components/customers/customer-client";
 
-function CustomersPageContent() {
-  return (
-    <>
-      <Header title="Customers" />
-      <CustomerClient />
-    </>
-  );
-}
-
 export default function CustomersPage() {
     return (
-        <Suspense fallback={
-            <>
-                <Header title="Customers" />
-                <Skeleton className="h-[700px] w-full" />
-            </>
-        }>
-            <CustomersPageContent />
-        </Suspense>
+        <>
+            <Header title="Customers" />
+            <Suspense fallback={<Skeleton className="h-[700px] w-full" />}>
+                <CustomerClient />
+            </Suspense>
+        </>
     )
 }
